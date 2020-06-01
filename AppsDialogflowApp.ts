a1a84e4bf075f2753bb9d03dfb0d811314c415ff
murtaza98 <m.patrawala@somaiya.edu>
2020-06-01 17:22:45 +0530

    configure app to only respond messages served by Bot

--- AppsDialogflowApp.ts
+++ AppsDialogflowApp.ts
@@ -2,13 +2,21 @@ import {
     IAppAccessors,
     IConfigurationExtend,
     IEnvironmentRead,
+    IHttp,
     ILogger,
+    IModify,
+    IPersistence,
+    IRead,
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IMessage, IPostMessageSent } from '@rocket.chat/apps-engine/definition/messages';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 import { AppSettings } from './AppSettings';
+import { ILivechatMessage, ILivechatRoom } from '@rocket.chat/apps-engine/definition/livechat';
+import { IUser } from '@rocket.chat/apps-engine/definition/users';
+import { RoomType } from '@rocket.chat/apps-engine/definition/rooms';
 
-export class AppsDialogflowApp extends App {
+export class AppsDialogflowApp extends App implements IPostMessageSent {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
     }
@@ -20,5 +28,41 @@ export class AppsDialogflowApp extends App {
 
     public async extendConfiguration(configuration: IConfigurationExtend): Promise<void> {
         AppSettings.forEach((setting) => configuration.settings.provideSetting(setting));
+    }
+
+    public async executePostMessageSent(message: IMessage,
+                                        read: IRead,
+                                        http: IHttp,
+                                        persistence: IPersistence,
+                                        modify: IModify): Promise<void> {
+
+        const SettingBotUsername: string = await this.getAppSetting(read, 'Lc-Bot-Username');
+        if (message.sender.username === SettingBotUsername) {
+            // this msg was sent by the Bot itself, so no need to respond back
+            return;
+        } else if (message.room.type !== RoomType.LIVE_CHAT) {
+            // check whether this is a Livechat message
+            return;
+        }
+
+        const lmessage: ILivechatMessage = message;
+        const lroom: ILivechatRoom = lmessage.room as ILivechatRoom;
+        const LcAgent: IUser = lroom.servedBy ? lroom.servedBy : message.sender;
+
+        // check whether the bot is currently handling the Visitor, if not then return back
+        if (SettingBotUsername !== LcAgent.username) {
+            return;
+        }
+
+        const BotMessage = 'Hello from Bot';
+
+        const builder = modify.getNotifier().getMessageBuilder();
+        builder.setRoom(message.room).setText(BotMessage).setSender(LcAgent);
+        modify.getCreator().finish(builder);
+
+    }
+
+    public async getAppSetting(read: IRead, id: string): Promise<any> {
+        return (await read.getEnvironmentReader().getSettings().getById(id)).value;
     }
 }
