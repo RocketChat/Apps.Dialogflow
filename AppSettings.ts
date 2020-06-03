d259dfe002f344752b464db990107fe554b63046
murtaza98 <m.patrawala@somaiya.edu>
2020-06-03 23:09:42 +0530

    removed unwanted dialogflow settings

--- AppSettings.ts
+++ AppSettings.ts
@@ -1,5 +1,4 @@
 import { ISetting, SettingType} from '@rocket.chat/apps-engine/definition/settings';
-import { createSign } from 'crypto';
 
 export const AppSettings: Array<ISetting> = [
     {
@@ -19,27 +18,11 @@ export const AppSettings: Array<ISetting> = [
         required: true,
     },
     {
-        id: 'Dialogflow-Private-Key-Id',
-        public: true,
-        type: SettingType.STRING,
-        packageValue: '',
-        i18nLabel: 'Dialogflow Private Key Id',
-        required: true,
-    },
-    {
         id: 'Dialogflow-Client-Email',
         public: true,
         type: SettingType.STRING,
         packageValue: '',
         i18nLabel: 'Dialogflow Client Email',
-        required: true,
-    },
-    {
-        id: 'Dialogflow-Client-Id',
-        public: true,
-        type: SettingType.STRING,
-        packageValue: '',
-        i18nLabel: 'Dialogflow Client Id',
         required: true,
     },
     {
