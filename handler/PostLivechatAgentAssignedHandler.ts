36a42959383094d831c8804513581e80dd794bbc
murtaza98 <m.patrawala@somaiya.edu>
2020-06-24 00:25:45 +0530

    fix tslint errors

--- handler/PostLivechatAgentAssignedHandler.ts
+++ handler/PostLivechatAgentAssignedHandler.ts
@@ -1,9 +1,9 @@
 import { ILivechatEventContext, ILivechatRoom } from '@rocket.chat/apps-engine/definition/livechat';
 
 import { IHttp, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
-import { AppPersistence } from '../lib/persistence';
-import { getAppSetting } from '../helper';
 import { AppSettingId } from '../AppSettings';
+import { getAppSetting } from '../helper';
+import { AppPersistence } from '../lib/persistence';
 
 export class PostLivechatAgentAssignedHandler {
     constructor(private context: ILivechatEventContext,
