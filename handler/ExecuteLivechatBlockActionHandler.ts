326e2007387ad7bfb626d2cbab3adc3092c01f60
murtaza98 <m.patrawala@somaiya.edu>
2020-09-02 22:17:50 +0530

    UiKit support - support for rich messages

---
+++ handler/ExecuteLivechatBlockActionHandler.ts
@@ -0,0 +1,47 @@
+import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { IApp } from '@rocket.chat/apps-engine/definition/IApp';
+import { ILivechatRoom } from '@rocket.chat/apps-engine/definition/livechat';
+import { IUIKitResponse, UIKitLivechatBlockInteractionContext } from '@rocket.chat/apps-engine/definition/uikit';
+import { UIKitIncomingInteractionContainerType } from '@rocket.chat/apps-engine/definition/uikit/UIKitIncomingInteractionContainer';
+import { IUser } from '@rocket.chat/apps-engine/definition/users';
+import { AppSetting } from '../config/Settings';
+import { createLivechatMessage, deleteAllActionBlocks } from '../lib/Message';
+import { getAppSettingValue } from '../lib/Settings';
+
+export class ExecuteLivechatBlockActionHandler {
+    constructor(private readonly app: IApp,
+                private context: UIKitLivechatBlockInteractionContext,
+                private read: IRead,
+                private http: IHttp,
+                private persistence: IPersistence,
+                private modify: IModify) {}
+
+    public async run(): Promise<IUIKitResponse> {
+        try {
+            const interactionData = this.context.getInteractionData();
+
+            const { visitor: { token }, room, container: { id, type }, actionId } = interactionData;
+
+            if (type !== UIKitIncomingInteractionContainerType.MESSAGE || !room) { return this.context.getInteractionResponder().successResponse(); }
+
+            const { servedBy: { username = null } = {}, id: rid } = room as ILivechatRoom;
+            if (!username) { return this.context.getInteractionResponder().successResponse(); }
+
+            const DialogflowBotUsername: string = await getAppSettingValue(this.read, AppSetting.DialogflowBotUsername);
+            if (DialogflowBotUsername !== username) { return this.context.getInteractionResponder().successResponse(); }
+
+            const visitor = await this.read.getLivechatReader().getLivechatVisitorByToken(token);
+            if (!visitor) { return this.context.getInteractionResponder().successResponse(); }
+            const appUser = await this.read.getUserReader().getAppUser(this.app.getID()) as IUser;
+
+            await createLivechatMessage(rid, this.read, this.modify, { text: actionId }, visitor);
+
+            await deleteAllActionBlocks(this.modify, appUser, id);
+
+            return this.context.getInteractionResponder().successResponse();
+        } catch (error) {
+            this.app.getLogger().error(error);
+            return this.context.getInteractionResponder().errorResponse();
+        }
+    }
+}
--- lib/Message.ts
+++ lib/Message.ts
@@ -1,5 +1,7 @@
 import { IModify, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { IVisitor } from '@rocket.chat/apps-engine/definition/livechat';
 import { BlockElementType, BlockType, IActionsBlock, IButtonElement, TextObjectType } from '@rocket.chat/apps-engine/definition/uikit';
+import { IUser } from '@rocket.chat/apps-engine/definition/users';
 import { AppSetting } from '../config/Settings';
 import { IDialogflowMessage, IDialogflowQuickReplies } from '../enum/Dialogflow';
 import { Logs } from '../enum/Logs';
@@ -85,4 +87,47 @@ export const createMessage = async (rid: string, read: IRead,  modify: IModify, 
         .then((result) => resolve(result))
         .catch((error) => console.error(error));
     });
+};
+
+export const createLivechatMessage = async (rid: string, read: IRead,  modify: IModify, message: any, visitor: IVisitor ): Promise<any> => {
+    if (!message) {
+        return;
+    }
+
+    const botUserName = await getAppSettingValue(read, AppSetting.DialogflowBotUsername);
+    if (!botUserName) {
+        this.app.getLogger().error(Logs.EMPTY_BOT_USERNAME_SETTING);
+        return;
+    }
+
+    const room = await read.getRoomReader().getById(rid);
+    if (!room) {
+        this.app.getLogger().error(`${Logs.INVALID_ROOM_ID} ${rid}`);
+        return;
+    }
+
+    const msg = modify.getCreator().startLivechatMessage().setRoom(room).setVisitor(visitor);
+
+    const { text, attachment } = message;
+
+    if (text) {
+        msg.setText(text);
+    }
+
+    if (attachment) {
+        msg.addAttachment(attachment);
+    }
+
+    return new Promise(async (resolve) => {
+        modify.getCreator().finish(msg)
+        .then((result) => resolve(result))
+        .catch((error) => console.error(error));
+    });
+};
+
+export const deleteAllActionBlocks = async (modify: IModify, appUser: IUser, msgId: string) => {
+    const msgBuilder = await modify.getUpdater().message(msgId, appUser);
+    msgBuilder.setEditor(appUser);
+    msgBuilder.setBlocks(modify.getCreator().getBlockBuilder().getBlocks());
+    await modify.getUpdater().finish(msgBuilder);
 };
