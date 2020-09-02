144b2e8ebb4ae6aa7d0035c772becc9ca8f574c1
murtaza98 <m.patrawala@somaiya.edu>
2020-09-02 23:31:21 +0530

    Quick-Replies improvement
    - auto hide action-buttons when clicked
    - purge block-id property in quick-replies-options
    - define interfaces for quick-replies-options

--- lib/Message.ts
+++ lib/Message.ts
@@ -1,7 +1,9 @@
 import { IModify, IRead } from '@rocket.chat/apps-engine/definition/accessors';
-import { BlockElementType, ButtonStyle, ConditionalBlockFiltersEngine, IButtonElement, TextObjectType } from '@rocket.chat/apps-engine/definition/uikit';
+import { IVisitor } from '@rocket.chat/apps-engine/definition/livechat';
+import { BlockElementType, BlockType, IActionsBlock, IButtonElement, TextObjectType } from '@rocket.chat/apps-engine/definition/uikit';
+import { IUser } from '@rocket.chat/apps-engine/definition/users';
 import { AppSetting } from '../config/Settings';
-import { IDialogflowMessage, IDialogflowQuickReplies } from '../enum/Dialogflow';
+import { IDialogflowMessage, IDialogflowQuickReplies, IDialogflowQuickRepliesOptions } from '../enum/Dialogflow';
 import { Logs } from '../enum/Logs';
 import { getAppSettingValue } from './Settings';
 
@@ -9,33 +11,23 @@ export const createDialogflowMessage = async (rid: string, read: IRead,  modify:
     const { messages = [] } = dialogflowMessage;
 
     for (const message of messages) {
-        const { text, options, blockId } = message as IDialogflowQuickReplies;
+        const { text, options } = message as IDialogflowQuickReplies;
 
         if (text && options) {
             // message is instanceof IDialogflowQuickReplies
-            const actions: Array<IButtonElement> = options.map((payload: any) => ({
+            const elements: Array<IButtonElement> = options.map((payload: IDialogflowQuickRepliesOptions) => ({
                 type: BlockElementType.BUTTON,
                 text: {
                     type: TextObjectType.PLAINTEXT,
                     text: payload.text,
                 },
-                actionId: payload.actionId ? payload.actionId : undefined,
-                value: payload.value ? payload.value : undefined,
-                style: payload.buttonStyle === 'danger' ? ButtonStyle.DANGER : undefined || payload.buttonStyle === 'primary' ? ButtonStyle.PRIMARY : undefined,
+                actionId: payload.actionId ? payload.actionId : payload.text,
+                ...payload.buttonStyle && { style: payload.buttonStyle },
             } as IButtonElement));
 
-            const blocks = modify.getCreator().getBlockBuilder();
-            const innerBlocks = modify.getCreator().getBlockBuilder();
+            const actionsBlock: IActionsBlock = { type: BlockType.ACTIONS, elements };
 
-            blocks.addConditionalBlock(
-                innerBlocks.addActionsBlock({
-                    blockId: blockId ? blockId : undefined,
-                    elements: actions,
-                }),
-                { engine: [ConditionalBlockFiltersEngine.LIVECHAT] },
-            );
-
-            await createMessage(rid, read, modify, { text, blocks });
+            await createMessage(rid, read, modify, { text, actionsBlock });
         } else {
             // message is instanceof string
             if ((message as string).trim().length > 0) {
@@ -69,14 +61,20 @@ export const createMessage = async (rid: string, read: IRead,  modify: IModify, 
     }
 
     const msg = modify.getCreator().startMessage().setRoom(room).setSender(sender);
-    const { text, blocks } = message;
+
+    const { text, actionsBlock, attachment } = message;
 
     if (text) {
         msg.setText(text);
     }
 
-    if (blocks) {
-        msg.addBlocks(blocks);
+    if (attachment) {
+        msg.addAttachment(attachment);
+    }
+
+    if (actionsBlock) {
+        const { elements } = actionsBlock as IActionsBlock;
+        msg.addBlocks(modify.getCreator().getBlockBuilder().addActionsBlock({ elements }));
     }
 
     return new Promise(async (resolve) => {
@@ -84,4 +82,47 @@ export const createMessage = async (rid: string, read: IRead,  modify: IModify, 
         .then((result) => resolve(result))
         .catch((error) => console.error(error));
     });
+};
+
+export const createLivechatMessage = async (rid: string, read: IRead,  modify: IModify, message: any, visitor: IVisitor ): Promise<any> => {
+    if (!message) {
+        return;
+    }
+
+    const botUserName = await getAppSettingValue(read, AppSetting.DialogflowBotUsername);
+    if (!botUserName) {
+        this.app.getLogger().error(Logs.EMPTY_BOT_USERNAME_SETTING);
+        return;
+    }
+
+    const room = await read.getRoomReader().getById(rid);
+    if (!room) {
+        this.app.getLogger().error(`${Logs.INVALID_ROOM_ID} ${rid}`);
+        return;
+    }
+
+    const msg = modify.getCreator().startLivechatMessage().setRoom(room).setVisitor(visitor);
+
+    const { text, attachment } = message;
+
+    if (text) {
+        msg.setText(text);
+    }
+
+    if (attachment) {
+        msg.addAttachment(attachment);
+    }
+
+    return new Promise(async (resolve) => {
+        modify.getCreator().finish(msg)
+        .then((result) => resolve(result))
+        .catch((error) => console.error(error));
+    });
+};
+
+export const deleteAllActionBlocks = async (modify: IModify, appUser: IUser, msgId: string) => {
+    const msgBuilder = await modify.getUpdater().message(msgId, appUser);
+    msgBuilder.setEditor(appUser);
+    msgBuilder.setBlocks(modify.getCreator().getBlockBuilder().getBlocks());
+    await modify.getUpdater().finish(msgBuilder);
 };
