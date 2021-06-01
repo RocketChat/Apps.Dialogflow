48c299c5d0fc31b0a501d85ab12a8a2fa8b7918c
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2021-06-01 10:56:01 +0530

    Fix bot typing

--- lib/BotTyping.ts
+++ lib/BotTyping.ts
@@ -4,13 +4,13 @@ const rooms = {};
 
 export async function botTypingListener(modify: IModify, rid: string, username: string ) {
     await removeBotTypingListener(rid);
-    const callback = modify.getNotifier().typing({ id: rid, username });
+    const callback = await modify.getNotifier().typing({ id: rid, username });
     rooms[rid] = callback;
 }
 
 export async function removeBotTypingListener(rid: string) {
     if (rooms[rid]) {
-        (await rooms[rid])();
+        await rooms[rid]();
         delete rooms[rid];
     }
 }
