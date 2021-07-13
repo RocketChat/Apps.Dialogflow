bcb0034cfeb6bac935694679e47e3c3764db43e0
AlexanderKanakis <kanaxbusiness@gmail.com>
2021-07-13 18:02:04 +0300

    Revert "Rearranged response handling"
    This reverts commit 06f0e1ac7e3802946f26ec0cda7b19737959f6d2.

--- handler/PostMessageSentHandler.ts
+++ handler/PostMessageSentHandler.ts
@@ -104,6 +104,10 @@ export class PostMessageSentHandler {
             return;
         }
 
+        handlePayloadActions(this.read, this.modify, this.http, this.persistence, rid, visitorToken, response);
+
+        handleParameters(this.read, this.modify, this.persistence, this.http, rid, visitorToken, response);
+
         const createResponseMessage = async () => await createDialogflowMessage(rid, this.read, this.modify, response);
 
         // synchronous handover check
@@ -114,8 +118,6 @@ export class PostMessageSentHandler {
         }
 
         await createResponseMessage();
-        await handlePayloadActions(this.read, this.modify, this.http, this.persistence, rid, visitorToken, response);
-        await handleParameters(this.read, this.modify, this.persistence, this.http, rid, visitorToken, response);
         await this.handleBotTyping(rid, response);
 
         return resetFallbackIntent(this.read, this.modify, rid);
