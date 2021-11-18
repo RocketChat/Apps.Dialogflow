6cfe8710d90d90767223e5b7748c41449f8b4355
Marcelo Schmidt <marcelo.schmidt@rocket.chat>
2021-11-18 09:03:31 -0300

    Update config/Settings.ts
    Co-authored-by: Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>

--- config/Settings.ts
+++ config/Settings.ts
@@ -39,7 +39,7 @@ export const settings: Array<ISetting> = [
         id: AppSetting.DialogflowProjectId,
         public: true,
         type: SettingType.STRING,
-        packageValue: 'draft',
+        packageValue: '',
         i18nLabel: 'dialogflow_project_id',
         required: true,
     },
